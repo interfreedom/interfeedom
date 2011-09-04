c2b57511585fd831dcd927ed4df789b5f33f155b
Jack Smentyna <djsmentya@gmail.com>
2011-09-04 22:06:22 +0300

    app sceleton #29239

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interfreedom::Application.config.secret_token = 'ba6ecf51a45c0a2624b359aa75d4bc6a65b3835eaa01e7de0038a0b94a1b461d637aef2b12fb2f367115b9bfbe28648fabf007e0a7a160ddfe041045232b3a70'
